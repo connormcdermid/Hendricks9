8e81761a496d3ddf2508397e0811b8eeca4dd593
frankgomes <frank.gomes@mcvts.org>
2019-02-16 18:16:07 -0500

    Trying out Kotlin for this next lab (CornUniversity in Kotlin).
    Signed-off-by: frankgomes <frank.gomes@mcvts.org>

--- src/CornUniversity.kt
+++ src/CornUniversity.kt
@@ -1,3 +1,5 @@
+import java.util.Scanner
+
 /**
  * @author Frank Gomes
  * @version 0.1
@@ -6,7 +8,31 @@
  * @extra
  **/
 
+
 fun main() {
-    val eyeah = 'c'
-    print(eyeah)
+    var kbd = Scanner(System.`in`)
+    println("Which college would you like to see?\n 1. MIT\n 2. Harvard\n 3. CCM")
+    var input = kbd.nextInt()
+    when (input) {
+        1 -> mit()
+        2 -> harvard()
+        3 -> ccm()
+        else -> {
+            print("That was not a recognized input.")
+            main()
+        }
+    }
+}
+
+fun mit() {
+    print("Massachusetts Institute of Technology\n2019 Enrollment: 2,024\nPercentage of women: 43%\nUndergraduate tuition: $45,278\nFun fact: Professor Anant Agarwal has a nice mustache.")
+
+}
+
+fun harvard() {
+
+}
+
+fun ccm() {
+
 }
