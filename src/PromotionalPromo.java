23fe3bcb6d0c5a40ea33388367bcee56dcd2a03b
BuildTools <unconfigured@null.spigotmc.org>
2019-02-17 10:15:35 -0500

    Did more work on MidYear Project from Classroom.

--- src/PromotionalPromo.java
+++ src/PromotionalPromo.java
@@ -14,6 +14,7 @@ public class PromotionalPromo {
     public static Scanner kbd = new Scanner(System.in);
 
     public static void main(String[] args) {
+        divider(14);
         // Here's those "nested loops" as desired for the logo
         for (int a = 0; a < 2; a++) {
             System.out.print("     .  .      .  .");
@@ -46,9 +47,42 @@ public class PromotionalPromo {
         System.out.print("\n");
     }
 
+    // Interactive menu for accessing employees, catalogue, and cart
     private static void menu() {
-        System.out.println("What would you like to do?\n 1. See our employees\n 2. See the catalogue\n 3. ");
+        System.out.println("What would you like to do?\n 1. See our employees\n 2. See the catalogue\n 3. Go to your cart\n 4. Exit");
+        // Gets user input
         String input = kbd.next();
+        // Selects the appropriate option
+        switch (input) {
+            case "1":
+                divider(10);
+                employees();
+                break;
+            case "2":
+                divider(10);
 
+                break;
+            case "3":
+                divider(10);
+
+                break;
+            case "4":
+                System.out.println("Goodbye, and thank you for shopping with Progress Fashions.");
+                System.exit(0);
+            default:
+                System.out.println("That was not a recognized input. Please try again.");
+                menu();
+                break;
+        }
+    }
+
+    // Displays our esteemed employees
+    private static void employees() {
+        System.out.println("Employees:");
+        System.out.println(" CEO & Founder: Poojit \"Puget\" Tummalapalli");
+        System.out.println(" Lead Designer: Marc Campos");
+        System.out.println(" Head of Production: Agranya Ketha");
+        System.out.println(" Royalties and like nothing else: Harish \"Chris Hart\" Sundar");
+        divider(10);
     }
 }
