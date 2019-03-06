a2fa7770b2e796a3d9a0a07c32604226a59a6130
frankgomes <frank.gomes@mcvts.org>
2019-03-06 09:18:15 -0500

    Added extra to 6.7.

--- src/FillenniumMalcon.java
+++ src/FillenniumMalcon.java
@@ -2,20 +2,57 @@
  author: Scrolling cursor
  lab: 6.6 Millenium Falcon Project
  date: 28-02-19
- extra: generates "Star Wars name on the fly
+ extra: progress bar
  */
 
 import java.util.Random;
+import me.tongfei.progressbar.*;
 
 public class FillenniumMalcon {
 
-    private static Random
+    private static Random rand = new Random();
 
     public static void main(String[] args) {
         System.out.println("Millennium Falcon Launch Simulation");
         System.out.println("System Navigator: " + nameGen("Frank", "Gomes", "progress", "Mount Olive"));
+        try (ProgressBar pb = new ProgressBar("Disengaging fuel pumps:", 100)) {
+            pb.stepBy(15);
+            Sleep(500);
+            pb.stepBy(15);
+            Sleep(500);
+            pb.stepBy(15);
+            Sleep(500);
+            pb.stepBy(15);
+            Sleep(500);
+            pb.stepTo(100);
+        }
+        try (ProgressBar pb2 = new ProgressBar("Pressurizing cabins:   ", 100)) {
+            pb2.stepBy(10);
+            Sleep(108);
+            pb2.stepBy(20);
+            Sleep(228);
+            pb2.stepBy(20);
+            Sleep(300);
+            pb2.stepBy(10);
+            Sleep(500);
+            pb2.stepBy(15);
+            Sleep(400);
+            pb2.stepBy(5);
+            Sleep(100);
+            pb2.stepTo(100);
+        }
     }
 
+    // Generates the wait in between the progress bar advances
+    private static void Sleep(int wait) {
+        try {
+            Thread.sleep(wait);
+        } catch (InterruptedException e) {
+            // Don't do anything about it
+        }
+    }
+
+    @SuppressWarnings("SameParameterValue")
     private static String nameGen(String firstName, String lastName, String maidenName, String birthTown) {
         if (firstName.equals("Tejas")) {
             System.out.println("No");
@@ -29,34 +66,38 @@ public class FillenniumMalcon {
         // Same reasoning for String.valueOf() as used above.
         String swLastName = String.valueOf(maidenName.charAt(0)) + String.valueOf(maidenName.charAt(1)) + String.valueOf(birthTown.charAt(0)) + String.valueOf(birthTown.charAt(1)) + String.valueOf(birthTown.charAt(2));
 
-        String finalName = swFirstName + " " + swLastName;
-
         // Returns finished name
-        return finalName;
+        return swFirstName + " " + swLastName;
     }
 
 
-    private static int r2d2() {
-
+    private static boolean r2d2() {
+        // If "system efficiency" (random number from rand.nextInt()) is greater than or equal to 40%, the launch is a go.
+        return (rand.nextInt() >= 40);
     }
 
-    private static int c3p0() {
-
+    private static boolean c3p0() {
+        // If "system efficiency" (random number from rand.nextInt()) is greater than or equal to 50%, the launch is a go.
+        return (rand.nextInt() >= 50);
     }
 
-    private static int han() {
-
+    private static boolean han() {
+        // If "system efficiency" (random number from rand.nextInt()) is greater than or equal to 30%, the launch is a go.
+        return (rand.nextInt() >= 30);
     }
 
-    private static int luke() {
-
+    private static boolean luke() {
+        // If "system efficiency" (random number from rand.nextInt()) is greater than or equal to 20%, the launch is a go.
+        return (rand.nextInt() >= 20);
     }
 
-    private static int chewy() {
-
+    private static boolean chewy() {
+        // If "system efficiency" (random number from rand.nextInt()) is greater than or equal to 35%, the launch is a go.
+        return (rand.nextInt() >= 35);
     }
 
-    private static int leia() {
-
+    private static boolean leia() {
+        // If "system efficiency" (random number from rand.nextInt()) is greater than or equal to 45%, the launch is a go.
+        return (rand.nextInt() >= 45);
     }
 }
