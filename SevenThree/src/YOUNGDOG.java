1e87229e80ede38cad5a1bca6db0591edff5f78c
frankgomes <frank.gomes@mcvts.org>
2019-03-22 09:02:07 -0400

    Finished 7.3 from Classroom.

--- SevenThree/src/YOUNGDOG.java
+++ SevenThree/src/YOUNGDOG.java
@@ -1,8 +1,21 @@
+/*
+  author: Frank Gomes
+  lab: 7.3 Use and Append Sequential Files
+  date: 20-03-19
+  extra: Prints the time taken for OLDDOG to execute
+ */
+
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.PrintWriter;
+import java.util.NoSuchElementException;
 import java.util.Scanner;
 
+import static java.lang.Character.isWhitespace;
+import static java.lang.System.currentTimeMillis;
+
+// Puts initial information into Dog.txt
 class YOUNGDOG {
     public static File dogFile = new File("SevenThree/output/Dog.txt");
     public static void main(String[] args) {
@@ -23,11 +36,17 @@ class YOUNGDOG {
                 "Corky 5\n" +
                 "Sadie 6\n" +
                 "Alexis 1");
+        // Closes scanner to write changes
+        pw.close();
     }
 }
 
-class OLDDOG {
+// Reads Dog.txt
+class OLDDOG
+{
     public static void main(String[] args) {
+        // Time that execution began
+        long startTime = currentTimeMillis();
         Scanner dog = null;
         try {
             dog = new Scanner(YOUNGDOG.dogFile);
@@ -35,6 +54,55 @@ class OLDDOG {
             e.getStackTrace();
             e.getMessage();
         }
-
+        while (true) {
+            // Makes the next line a String called nextLine
+            String nextLine = null;
+            try {
+                nextLine = dog.nextLine();
+            } catch (NoSuchElementException e) {
+                // In the event the scanner is at the end of the file, when trying to do dog.nextLine(), it will throw
+                // a NoSuchElementException. The try/catch breaks the loop at the end of the file.
+                break;
+            }
+            // Keeps printing out each character of nextLine until it reaches the space before the age
+            for (int i = 0; i < nextLine.length(); i++) {
+                if (isWhitespace(nextLine.charAt(i)))
+                    break;
+                else
+                    System.out.print(nextLine.charAt(i));
+            }
+            System.out.print(": ");
+            // Checks if the age in nextLine is equal or greater than 7
+            if ((nextLine.lastIndexOf(" ") + 1) >= 7)
+                System.out.print("OLD DOG");
+            else
+                System.out.print("YOUNG DOG");
+            // Prints \n to prevent it all from going on one line
+            System.out.print("\n");
+        }
+        System.out.println("Done in " + (currentTimeMillis() - startTime)/1000 + " seconds.");
     }
 }
+
+// Appends new information
+class AppendDog {
+    public static void main(String[] args) {
+        // Declares PrintWriter pw to be assigned in the try/catch loop
+        PrintWriter pw = null;
+        // Try/catch statement to make PrintWriter for Dog.txt
+        try {
+            pw = new PrintWriter(new FileOutputStream(new File("SevenThree/output/Dog.txt"), true));
+        } catch (FileNotFoundException e) {
+            e.getStackTrace();
+            e.getMessage();
+        }
+        // Appends new dog info to Dog.txt
+        pw.print("Beatrice 3\n" +
+                "Tasha 4\n" +
+                "Patty 1\n");
+        // Closes scanner to write changes
+        pw.close();
+    }
+}
+
+// For part four, just run OLDDOG again
