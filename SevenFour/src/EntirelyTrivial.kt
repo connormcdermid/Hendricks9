0ccf3cb25cd4de7ef55425eab049b7ec1b202614
frankgomes <frank.gomes@mcvts.org>
2019-03-26 09:19:50 -0400

    Worked on 7.4 from Classroom.

--- SevenFour/src/EntirelyTrivial.kt
+++ SevenFour/src/EntirelyTrivial.kt
@@ -15,7 +15,7 @@ fun main() {
     val question2 = arrayOf("What is the capital of Peru?", " 1. Lima", " 2. São Paulo", " 3. Bogota", " 4. Mexico City", "1")
     val question3 = arrayOf("What was the most watched UK TV programme?", " 1. M*A*S*H", " 2. BBC's Sherlock", " 3. The Office", " 4. Eastenders", "")
     val question4 = arrayOf("What railway linked Moscow and Irkutsk in 1900?", " 1. Shinkansen Line", " 2. Trans-Siberian Railway", " 3. Sakhalin's Railway", " 4. Allegro Line", "2")
-    val question5 = arrayOf("What's another common name for the ", " 1. ", " 2. ", " 3. ", " 4. ", "")
+    val question5 = arrayOf("What's another common name for the water moccasin?", " 1. Brownmouth", " 2. Sea moccasin", " 3. Shoe Snake", " 4. Cottonmouth", "4")
 
     // Creates PrintWriter to write to score.txt
     val scoreFile = FileWriter("SevenFour/output/score.txt", true)
