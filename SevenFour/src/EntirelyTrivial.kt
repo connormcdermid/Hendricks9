0dca935d9ac92d85aa7ff78b24dfb89f335bb621
frankgomes <frank.gomes@mcvts.org>
2019-03-25 20:31:45 -0400

    Started 7.4 from Classroom.

--- SevenFour/src/EntirelyTrivial.kt
+++ SevenFour/src/EntirelyTrivial.kt
@@ -2,13 +2,16 @@
   author: Frank Gomes
   lab: 7.4 Trivial Pursuit Lab
   date: 20-03-19
-  extra: Prints the time taken for OLDDOG to execute
+  extra: Randomly selects questions
  */
 
 import java.io.File
+import java.io.PrintWriter
 
 class EntirelyTrivial {
     fun main() {
         val scoreFile = File("SevenFour/output/score.txt")
+        val pw = PrintWriter(scoreFile)
+
     }
 }
