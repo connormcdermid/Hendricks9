8dba398db263f7dc364220e7119462fc6cfa838b
BuildTools <unconfigured@null.spigotmc.org>
2019-03-25 22:48:45 -0400

    Worked on 7.4 from Classroom.

--- SevenFour/src/EntirelyTrivial.kt
+++ SevenFour/src/EntirelyTrivial.kt
@@ -1,17 +1,35 @@
+import java.io.File
+import java.io.FileWriter
+import java.io.PrintWriter
+import java.util.*
+
 /*
   author: Frank Gomes
   lab: 7.4 Trivial Pursuit Lab
-  date: 20-03-19
-  extra: Randomly selects questions
+  date: 22-03-19
+  extra:
  */
 
-import java.io.File
-import java.io.PrintWriter
+fun main() {
+    // Creates PrintWriter to write to score.txt
+    val scoreFile = FileWriter("SevenFour/output/score.txt", true)
+    val pw = PrintWriter(scoreFile)
+    // Adds a divider to the file to keep run entries separate
+    pw.println("---------------------")
+    // Creates Scanner to read user input
+    val kbd = Scanner(System.`in`)
+    // Gets user info
+    println("Please enter your first name.")
+    pw.print("\nName: " + kbd.next())
+    println("Please enter your last name.")
+    pw.print(" " + kbd.next())
+    // Introduces trivia show
+    println("Welcome to Generic Uncopyrighted Trivia Game!\nHow was your day?")
+    Thread.sleep(2000)
+    println("I don't care.")
 
-class EntirelyTrivial {
-    fun main() {
-        val scoreFile = File("SevenFour/output/score.txt")
-        val pw = PrintWriter(scoreFile)
 
-    }
+    // Closes PrintWriter & FileWriter
+    pw.close()
+    scoreFile.close()
 }
