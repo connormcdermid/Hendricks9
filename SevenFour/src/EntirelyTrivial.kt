e72407087402f84b1fb3fda62a09bc3b237b8197
frankgomes <frank.gomes@mcvts.org>
2019-03-28 20:04:44 -0400

    Worked on 7.4 from Classroom.

--- SevenFour/src/EntirelyTrivial.kt
+++ SevenFour/src/EntirelyTrivial.kt
@@ -13,9 +13,13 @@ fun main() {
     // Array containing all of the questions
     val question1 = arrayOf("How many years was rapper Daniel Hernandez originally sentenced to?", " 1. 40 years", " 2. 47 years", " 3. 25 years", " 4. 69 Years", "2")
     val question2 = arrayOf("What is the capital of Peru?", " 1. Lima", " 2. São Paulo", " 3. Bogota", " 4. Mexico City", "1")
-    val question3 = arrayOf("What was the most watched UK TV programme?", " 1. M*A*S*H", " 2. BBC's Sherlock", " 3. The Office", " 4. Eastenders", "")
+    val question3 = arrayOf("What was the most watched UK TV programme?", " 1. M*A*S*H", " 2. BBC's Sherlock", " 3. The Office", " 4. Eastenders", "4")
     val question4 = arrayOf("What railway linked Moscow and Irkutsk in 1900?", " 1. Shinkansen Line", " 2. Trans-Siberian Railway", " 3. Sakhalin's Railway", " 4. Allegro Line", "2")
     val question5 = arrayOf("What's another common name for the water moccasin?", " 1. Brownmouth", " 2. Sea moccasin", " 3. Shoe Snake", " 4. Cottonmouth", "4")
+    val question6 = arrayOf("Leader of terrorist group al-Qaeda, Osama bin Laden, was killed in what year?", " 1. 2004", " 2. 1994", " 3. 2011", " 4. 2013", "3")
+    val question7 = arrayOf("Which country is the city Tbilisi located in?", " 1. Georgia", " 2. Romania", " 3. Greece", " 4. Peru", "1")
+    val question8 = arrayOf("The games convention PAX West is held where?", " 1. Boston, MA", " 2. Seattle, WA", " 3. San Antonio, TX", " 4. Everett, WA", "2")
+    val question9 = arrayOf("Rapper Gunna released which album in 2018?", " 1. Drip Too Hard", " 2. Drip or Drown", " 3. Drip Harder", " 4. ", "")
 
     // Creates PrintWriter to write to score.txt
     val scoreFile = FileWriter("SevenFour/output/score.txt", true)
--- old/src/TestFiveTest.java
+++ old/src/TestFiveTest.java
@@ -1,13 +1,44 @@
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.text.DecimalFormat;
 import java.util.Scanner;
-
-import static java.lang.Integer.parseInt;
 
 public class TestFiveTest {
     public static void main (String[] args) {
-        String attendance = "32 students";
-        int students = parseInt(String.valueOf(attendance.charAt(0)) + String.valueOf(attendance.charAt(1)));
-        System.out.println(students + " students will prepare " + (students*3) + " exam papers.");
+        String department;
+        int security;
+        double salary = 0, total = 0;
+        File file;
+        Scanner reply;
+        DecimalFormat decFor = new DecimalFormat("$#,###.00");
+
+        try {
+            file = new File("src/work.txt");
+            reply = new Scanner(file);
+
+            while (reply.hasNext())
+            {
+                // You're only able to make changes from here...
+                department = reply.next();
+                System.out.println("Department: " + department);
+                security = reply.nextInt();
+                System.out.println("Security #:" + security);
+                for (int i = 0; i < 3; i++) {
+                    System.out.print("\nPrevious salaries: ");
+                    salary = reply.nextDouble();
+                    System.out.print(salary);
+                    if (reply.hasNextDouble())
+                        System.out.print(", ");
+                    total += salary;
+                    System.out.println("Average salary: " + total/3);
+                }
+                // ...to here.
+            }
+            reply.close();
+        }
+        catch (FileNotFoundException e)
+        {
+            System.out.println(e.getMessage());
+        }
     }
 }
