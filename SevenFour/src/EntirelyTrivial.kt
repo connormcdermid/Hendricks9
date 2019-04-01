5c40529a19535b7d48a55d48a3defa7218397d6f
BuildTools <unconfigured@null.spigotmc.org>
2019-04-01 19:32:39 -0400

    Worked on 7.4 from Classroom.

--- SevenFour/src/EntirelyTrivial.kt
+++ SevenFour/src/EntirelyTrivial.kt
@@ -30,20 +30,29 @@ fun main() {
     // Creates PrintWriter to write to score.txt
     val scoreFile = FileWriter("SevenFour/output/score.txt", true)
     val pw = PrintWriter(scoreFile)
+
     // Adds a divider to the file to keep run entries separate
     pw.println("---------------------")
+
     // Creates Scanner to read user input
     val kbd = Scanner(System.`in`)
+
     // Gets user info
     println("Please enter your first name.")
     pw.print("\nName: " + kbd.next())
     println("Please enter your last name.")
     pw.print(" " + kbd.next())
+
     // Introduces trivia show
     println("Welcome to Generic Uncopyrighted Trivia Game!\nHow was your day?")
     Thread.sleep(2000)
     println("I don't care.")
-
+    print("\nAre you ready?")
+    Thread.sleep(500)
+    print(" No?")
+    Thread.sleep(500)
+    print(" Too bad.")
+    //
 
     // Closes PrintWriter & FileWriter
     pw.close()
