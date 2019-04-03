d1bc8162a808fa389e7821fcd1c2e9e08871fa69
frankgomes <frank.gomes@mcvts.org>
2019-04-03 09:20:09 -0400

    Arghhhhhhhh.

--- EightOne/src/GoArray.java
+++ EightOne/src/GoArray.java
@@ -1,3 +1,16 @@
-public class GoArray {
+import java.util.Arrays;
+import java.util.Random;
 
+public class GoArray {
+    public static void main(String[] args) {
+        Random rand = new Random();
+        int[] randNums = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+        for (int i = 0; i < randNums.length; i++)
+            randNums[i] = rand.nextInt(11);
+        System.out.println(Arrays.toString(randNums));
+        System.out.print("\n[");
+        for (int i = 0; i < randNums.length; i++)
+            System.out.print(randNums[9-i] + ", ");
+        System.out.print("\b\b]");
+    }
 }
