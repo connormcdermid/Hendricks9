b8b804eeba3fedd7cf6d09ff082286e0208ff6d0
frankgomes <frank.gomes@mcvts.org>
2019-04-24 20:24:35 -0400

    Started 8.8 from Classroom.

---
+++ EightEight/src/Databased god.kt
@@ -0,0 +1,4 @@
+fun main() {
+    val nameArray = arrayOf("Barney Fife","Luke Skywalker","Cruella Deville","Sally Smith","Marsha Brady","Gomez Addams","John Adams","Marie Antoinette","Albert Einstein","Mick Jagger","Mickey Mouse")
+    val addressArray = arrayOf("10 Warbler Rd.","R2 Death Star Ave.","123 Witchey Way","3862 Street Blvd.","1970 Groovy Street","1 Spooky Circle","3445 Presidential Plaza","2 Rue Morgue","7 Relativity Rd.","100 Rolling Stone Path","2 Disney Lane")
+}
