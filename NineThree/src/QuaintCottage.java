c02d792112d94e7d94e461b4ef53c4f3f262eb94
BuildTools <unconfigured@null.spigotmc.org>
2019-06-09 23:42:17 -0400

    Finished 9.3 from Classroom.

--- NineThree/src/QuaintCottage.java
+++ NineThree/src/QuaintCottage.java
@@ -1,3 +1,10 @@
+/*
+  author: Frank Gomes
+  lab: 9.3 Worksheet Drawings
+  date: 08-06-19
+  extra: Blades of grass
+ */
+
 import javax.swing.*;
 import java.awt.*;
 
@@ -5,16 +12,27 @@ class QuaintCottage extends JPanel {
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
         // Drawings go here!
+        Graphics2D g2 = (Graphics2D) g;
         // Ground
-        g.setColor(Color.green);
-        g.fillRect(0,200,400,200);
+        g2.setColor(Color.green);
+        g2.fillRect(0,200,400,200);
         // Grass blades
-        g.setColor(new Color(0,120,0));
-        g.fillPolygon(new int[] {20,25,30,35,40,45,50}, new int[] {200,170,200,170,200,170,200}, 7);
-        g.fillPolygon(new int[] {320,325,330,335,340,345,350}, new int[] {200,170,200,170,200,170,200}, 7);
+        g2.setColor(new Color(0,120,0));
+        g2.fillPolygon(new int[] {20,25,30,35,40,45,50}, new int[] {200,170,200,170,200,170,200}, 7);
+        g2.fillPolygon(new int[] {320,325,330,335,340,345,350}, new int[] {200,170,200,170,200,170,200}, 7);
         // House
-        g.setColor(new Color(205,51,1));
-        g.fillRect(100,100,200,100);
+        g2.setColor(new Color(205,51,1));
+        g2.fillRect(100,100,200,100);
+        // Roof
+        g2.setStroke(new BasicStroke(4));
+        g2.setColor(Color.BLACK);
+        g2.drawPolygon(new int[] {100, 200, 300}, new int[] {100, 50, 100}, 3);
+        // Door
+        g2.setColor(Color.YELLOW);
+        g2.fillRect(185,150,30,50);
+        // Windows
+        g2.fillRect(130,140,35,35);
+        g2.fillRect(235,140,35,35);
     }
 
     public static void main(String[] args) {
@@ -29,5 +47,42 @@ class QuaintCottage extends JPanel {
         // Centers the window on the screen
         f.setLocationRelativeTo(null);
         f.setVisible(true);
+        // Olympic Rings JFrame
+        JFrame f2 = new JFrame("Olympic Rings");
+        f2.add(new OlympicRings());
+        f2.setSize(1218,595);
+        f2.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        f2.setLocationRelativeTo(null);
+        f2.setVisible(true);
+        // Creepy Face JFrame
+        JFrame f3 = new JFrame("Creepy face");
+        f3.add(new CreepyFace());
+        f3.setSize(200,100);
+        f3.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        f3.setLocationRelativeTo(null);
+        f3.setVisible(true);
+    }
+}
+
+class OlympicRings extends JPanel {
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        ImageIcon rings = new ImageIcon("NineThree/olympic-rings.png");
+        rings.paintIcon(this,g,0,0);
+    }
+}
+
+class CreepyFace extends JPanel {
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        g.setColor(Color.YELLOW);
+        g.fillOval(0,0,200,100);
+        g.setColor(Color.BLUE);
+        g.fillOval(30,25,45,25);
+        g.fillOval(125,25,45,25);
+        g.setColor(Color.BLACK);
+        g.fillOval(95,30,10,40);
+        g.setColor(Color.PINK);
+        g.fillArc(75,60,50,20,0,-180);
     }
 }
